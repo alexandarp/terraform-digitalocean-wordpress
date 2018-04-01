4f97e29ebace9edd49834d32de3dce91aeb9e028
Alex Podobnik <alexandar.podobnik@gmail.com>
2018-04-01 14:20:01 -0400

    Update example.tf
    changing module name

--- example/example.tf
+++ example/example.tf
@@ -1,4 +1,4 @@
 module "example" {
-  source = "git::ssh://git@github.com/alexandarp/terraform-wordpress-digitalocean"
+  source = "git::ssh://git@github.com/alexandarp/terraform-digitalocean-wordpress"
   name = "example-wordpress-droplet"
 }

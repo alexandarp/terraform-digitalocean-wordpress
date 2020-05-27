6057350114d87dea02f903d0b2c9d754ea3b93e9
Alex Podobnik <alexandar.podobnik@gmail.com>
2020-05-26 20:05:24 -0400

    Upgrade to Terraform 0.12

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 resource "digitalocean_droplet" "wordpress" {
-  image  = "${var.image}"
-  name   = "${var.name}"
-  region = "${var.region}"
-  size   = "${var.size}"
-  backups = "${var.backups}"
-  monitoring = "${var.monitoring}"
-  ipv6 = "${var.ipv6}"
-  private_networking = "${var.private_networking}"
-  resize_disk = "${var.resize_disk}"
+  image              = var.image
+  name               = var.name
+  region             = var.region
+  size               = var.size
+  backups            = var.backups
+  monitoring         = var.monitoring
+  ipv6               = var.ipv6
+  private_networking = var.private_networking
+  resize_disk        = var.resize_disk
 }
+
